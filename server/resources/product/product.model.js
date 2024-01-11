f54b21204ff8f0071014e0deef047f6376d0308a
Ammar Hasan <ammarhasan011@gmail.com>
2024-01-11 21:13:47 +0100

    img saknades och rättade till stavfel

--- server/resources/product/product.model.js
+++ server/resources/product/product.model.js
@@ -5,11 +5,11 @@ const Joi = require("joi");
 //Mongoose schema
 const ProductSchema = new Schema(
   {
-    title: { type: String, require: true },
-    description: { type: String, require: true },
-    price: { type: Number, require: true },
-    image: { type: String, require: true },
-    inStock: { type: Number, require: true, default: 0 },
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    image: { type: String, required: true },
+    inStock: { type: Number, required: true, default: 0 },
     deleted: { type: Boolean, required: false, default: false },
   },
   // för att undvika att Mongoose lägger till ett __V för versionsinformation
@@ -18,13 +18,14 @@ const ProductSchema = new Schema(
 
 //Här skapas en Mongoose modell
 const ProductModel = models.product || model("product", ProductSchema);
-console.log("modell");
+// console.log("modell");
 
 //valideringsscheman för skapande produkter/validera inkommande data
 const ProductCreateValidationSchema = Joi.object({
   title: Joi.string().strict().required(),
   description: Joi.string().strict().required(),
   price: Joi.number().strict().required(),
+  image: Joi.string().uri().allow("image/png", "image/jpeg").required(),
   inStock: Joi.number().strict().required(),
 });
 // En schema för uppdateringar av produkter, men lägger krav på id & delete
