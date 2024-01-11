31ce761eb9cae869295d0cc50d63b4b465e62246
Ammar Hasan <ammarhasan011@gmail.com>
2024-01-11 13:46:51 +0100

    kopplade databasen

--- server/server.js
+++ server/server.js
@@ -1,6 +1,18 @@
 const express = require("express");
-// const mongoose = require("mongoose");
-require("dotenv").config();
-// mongodb+srv: ammarhasan011:<password>@cluster0.uyrlsu9.mongodb.net/
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+
 const app = express();
-app.listen(3000, () => console.log("Server is up and runninnn..."));
+
+//databas
+async function connectionDB() {
+  mongoose.set("strictQuery", true);
+  await mongoose.connect(process.env.MONGODB_CONNECTION_STRING);
+  app.listen(process.env.PORT || 4001, () =>
+    console.log("Database is up and runninnn...port4000")
+  );
+}
+
+connectionDB();
+
+app.listen(3000, () => console.log("Server is up and runninnn...port3000"));
