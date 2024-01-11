b2271516406fcf53067558d7e768546cd4176291
Ammar Hasan <ammarhasan011@gmail.com>
2024-01-11 15:48:47 +0100

    kommentarer

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const dotenv = require("dotenv").config();
 
 const app = express();
 
-//databas
+//koppling till databas som kör på port 4000
 async function connectionDB() {
   mongoose.set("strictQuery", true);
   await mongoose.connect(process.env.MONGODB_CONNECTION_STRING);
@@ -15,4 +15,5 @@ async function connectionDB() {
 
 connectionDB();
 
+// server
 app.listen(3000, () => console.log("Server is up and runninnn...port3000"));
