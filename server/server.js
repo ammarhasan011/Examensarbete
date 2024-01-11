1efa84e4bbf2b4440ad581bf1b44f28b04008c56
Ammar Hasan <ammarhasan011@gmail.com>
2024-01-11 13:27:46 +0100

    test

--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+// const mongoose = require("mongoose");
+require("dotenv").config();
+// mongodb+srv: ammarhasan011:<password>@cluster0.uyrlsu9.mongodb.net/
 const app = express();
-
 app.listen(3000, () => console.log("Server is up and runninnn..."));
