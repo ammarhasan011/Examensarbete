a2c17bbcda477fb729123053250eb2c8e5eaa70f
Ammar Hasan <ammarhasan011@gmail.com>
2024-01-12 22:31:24 +0100

    user router

---
+++ server/resources/user/user.router.js
@@ -0,0 +1,13 @@
+const { Router } = require("express");
+const { register, login, logout } = require("./user.controller");
+// const { UserCreateValidationSchema } = require("./user.model");
+
+const userRouter = Router();
+
+userRouter
+  //   .post("/users/register", validate(UserCreateValidationSchema), register)
+  .post("/users/register", register)
+  .post("/users/login", login)
+  .post("/users/logout", logout);
+
+module.exports = { userRouter };
