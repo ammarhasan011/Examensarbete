c0b8ee8677f4358b1eeafe5909fba1cee0e21746
Ammar Hasan <ammarhasan011@gmail.com>
2024-01-12 19:23:57 +0100

    user controller klar

--- server/resources/user/user.controller.js
+++ server/resources/user/user.controller.js
@@ -2,7 +2,7 @@ const { UserModel } = require("./user.model");
 const bcrypt = require("bcrypt");
 
 async function register(req, res) {
-  //kolla om user redan finns
+  //check if user already exists
   const existingUser = await UserModel.findOne({ email: req.body.email });
   if (existingUser) {
     return res.status(409).json("Email already registered");
@@ -29,4 +29,35 @@ async function login(req, res) {
   ) {
     return res.status(401).json("Wrong password or username");
   }
+
+  const user = existingUser.toJSON();
+  user._id = existingUser._id;
+  delete user.password;
+
+  // Check if user already is logged in
+  if (req.session._id) {
+    return res.status(200).json(user);
+  }
+
+  // Save info about the user to the session (an encrypted cookie stored on the client)
+  req.session = user;
+  res.status(200).json(user);
 }
+
+// Logout user & remove cookie & session
+async function logout(req, res) {
+  if (!req.session._id) {
+    return res.status(400).json("Cannot logout when you are not logged in");
+  }
+  req.session = null;
+  res.status(204).json(null);
+}
+
+async function authorize(req, res) {
+  if (!req.session._id) {
+    return res.status(401).json("You are not logged in");
+  }
+  res.status(200).json(req.session);
+}
+
+module.exports = { register, login, logout, authorize };
