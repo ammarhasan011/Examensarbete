7e3e741b3923581ba8039d708c6893d234dce486
Ammar Hasan <ammarhasan011@gmail.com>
2024-01-12 12:03:27 +0100

    använd Route för url

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/header/header";
+import Cart from "./components/cart/cart";
+import User from "./components/user/user";
+import Home from "./components/home";
+import Products from "./components/products/products";
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
-      <h1>Hello</h1>
-    </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/user" element={<User />} />
+      </Routes>
+    </Router>
   );
 };
 
