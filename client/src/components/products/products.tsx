82abbb406822096785c5568fb29e9093e1195108
Ammar Hasan <ammarhasan011@gmail.com>
2024-01-12 13:14:58 +0100

    axios för datan från DB

--- client/src/components/products/products.tsx
+++ client/src/components/products/products.tsx
@@ -1,4 +1,20 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 const Products = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/api/products")
+      .then((response) => {
+        setProducts(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching products", error);
+      });
+  }, []);
+
   return (
     <div>
       <h1>Products</h1>
