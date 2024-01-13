85eddebaa85208f925e378f7e365106687650e14
Ammar Hasan <ammarhasan011@gmail.com>
2024-01-13 05:58:47 +0100

    Tog bort databasen från server & importerat db

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
 const cookieSession = require("cookie-session");
+const { connectToDatabase } = require("./resources/db/db");
 
 const productRouter = require("./resources/product/product.router");
 const { userRouter } = require("./resources/user/user.router");
@@ -11,16 +11,7 @@ const app = express();
 // Middleware för att tolka JSON
 app.use(express.json());
 app.use(cors());
-
-//koppling till databas som kör på port 4000
-async function connectionDB() {
-  mongoose.set("strictQuery", true);
-  await mongoose.connect(process.env.MONGODB_CONNECTION_STRING);
-  app.listen(process.env.PORT || 4001, () =>
-    console.log("Database is up and runninnn...port4000")
-  );
-}
-connectionDB();
+connectToDatabase();
 
 app.use(
   cookieSession({
