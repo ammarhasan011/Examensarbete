313fcf93c81a52df773204b58e432030adeff931
Ammar Hasan <ammarhasan011@gmail.com>
2024-01-14 23:28:17 +0100

    syntaxfel,

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/header/header";
 import Cart from "./components/cart/cart";
-import SignIpForm from "./components/user-forms/signIn-form";
+import SignInForm from "./components/user-forms/signIn-form";
 import Home from "./components/home";
 import Products from "./components/products/products";
 import AboutUs from "./components/aboutUs/aboutUs";
@@ -17,7 +17,7 @@ const App = () => {
         <Route path="/products" element={<Products />} />
         <Route path="/about-us" element={<AboutUs />} />
         <Route path="/cart" element={<Cart />} />
-        <Route path="/sign-in" element={<SignIpForm />} />
+        <Route path="/sign-in" element={<SignInForm />} />
         <Route path="/sign-up" element={<SignUpForm />} />
       </Routes>
       <Footer />
