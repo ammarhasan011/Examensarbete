30d24a08cf7947fb7b98d806cd865b45ae6781d6
Ammar Hasan <ammarhasan011@gmail.com>
2024-01-14 21:04:21 +0100

    url & namn gevning

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/header/header";
 import Cart from "./components/cart/cart";
-import User from "./components/user-forms/signIn-form";
+import SignIpForm from "./components/user-forms/signIn-form";
 import Home from "./components/home";
 import Products from "./components/products/products";
 import AboutUs from "./components/aboutUs/aboutUs";
 import Footer from "./components/footer/footer";
-import RegisterForm from "./components/user-forms/signUp-form";
+import SignUpForm from "./components/user-forms/signUp-form";
 
 const App = () => {
   return (
@@ -17,8 +17,8 @@ const App = () => {
         <Route path="/products" element={<Products />} />
         <Route path="/about-us" element={<AboutUs />} />
         <Route path="/cart" element={<Cart />} />
-        <Route path="/sign-in" element={<User />} />
-        <Route path="/sign-up" element={<RegisterForm />} />
+        <Route path="/sign-in" element={<SignIpForm />} />
+        <Route path="/sign-up" element={<SignUpForm />} />
       </Routes>
       <Footer />
     </Router>
