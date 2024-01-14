47a0ce84b579f4524ea046ce4606946c046c3707
Ammar Hasan <ammarhasan011@gmail.com>
2024-01-14 15:43:53 +0100

    lagt till ikon och fixat formens storlek

--- client/src/components/user/user.tsx
+++ client/src/components/user/user.tsx
@@ -1,10 +1,24 @@
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
+import Avatar from "@mui/material/Avatar";
+import PersonSharpIcon from "@mui/icons-material/PersonSharp";
 
 const User = () => {
+  const paperStyle = {
+    padding: 20,
+    height: "70vh",
+    width: 300,
+    margin: "30px auto",
+  };
+
   return (
     <Grid>
-      <Paper>logga in</Paper>
+      <Paper elevation={10} style={paperStyle}>
+        <Avatar>
+          <PersonSharpIcon />
+        </Avatar>
+        <h2> logga in</h2>
+      </Paper>
     </Grid>
   );
 };
