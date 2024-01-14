8e0b0c5c34dec57e592cecaa37237f0e9f530167
Ammar Hasan <ammarhasan011@gmail.com>
2024-01-14 16:40:14 +0100

    input fält

--- client/src/components/user/user.tsx
+++ client/src/components/user/user.tsx
@@ -2,6 +2,8 @@ import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 import Avatar from "@mui/material/Avatar";
 import PersonSharpIcon from "@mui/icons-material/PersonSharp";
+import TextField from "@mui/material/TextField";
+import "./user.css";
 
 const User = () => {
   const paperStyle = {
@@ -11,20 +13,31 @@ const User = () => {
     margin: "30px auto",
   };
 
-  const avatarStyle = {
-    backgroundcolor: "pink",
-  };
+  const avatarStyle = { backgroundColor: "#1976d2" };
 
   return (
     <Grid>
       <Paper elevation={10} style={paperStyle}>
-        {/* aling funkar inte  */}
-        <Grid alignItems="center">
+        <Grid container direction="column" alignItems="center">
           <Avatar style={avatarStyle}>
             <PersonSharpIcon />
           </Avatar>
           <h2> logga in</h2>
         </Grid>
+        <TextField
+          id="standard-basic"
+          label="Username"
+          variant="standard"
+          fullWidth
+          required
+        />
+        <TextField
+          id="standard-basic"
+          label="Password"
+          variant="standard"
+          fullWidth
+          required
+        />
       </Paper>
     </Grid>
   );
