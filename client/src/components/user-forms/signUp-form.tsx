d33d0db4862a4babaaf54679f2f8c49ce708c3e6
Ammar Hasan <ammarhasan011@gmail.com>
2024-01-14 22:30:50 +0100

    kopplat min sign up med backenden

--- client/src/components/user-forms/signUp-form.tsx
+++ client/src/components/user-forms/signUp-form.tsx
@@ -7,6 +7,7 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import { Link as RouterLink } from "react-router-dom";
+import axios from "axios";
 
 const SignUpForm = () => {
   const paperStyle = {
@@ -19,6 +20,48 @@ const SignUpForm = () => {
   const headerStyle = { margin: 0 };
   const avatarStyle = { backgroundColor: "#1976d2" };
   const buttonStyle = { marginTop: 40, margin: "8px 0" };
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //Security check to avoid 'null' error
+  const handleRegistration = async (
+    //types up event so React understands
+    event: React.FormEvent<HTMLFormElement>
+  ) => {
+    event.preventDefault();
+
+    if (!event.target) {
+      return;
+    }
+
+    // Typing function to tell TypeScript that this is a form element
+    const target = event.target as typeof event.target & {
+      firstName: { value: string };
+      lastName: { value: string };
+      email: { value: string };
+      password: { value: string };
+    };
+
+    // Get the form data
+    const formData = {
+      firstName: target.firstName.value,
+      lastName: target.lastName.value,
+      email: target.email.value,
+      password: target.password.value,
+    };
+
+    try {
+      //Send a POST request to backend
+      const response = await axios.post(
+        "http://localhost:3000/api/users/register",
+        formData
+      );
+
+      // Console the data from backend
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
   return (
     <Grid>
@@ -39,10 +82,11 @@ const SignUpForm = () => {
             Fyll i formulären för att skapa ett konto!
           </Typography>
         </Grid>
-        <form action="">
+        <form onSubmit={handleRegistration}>
           {" "}
           <TextField
             id="standard-basic"
+            name="firstName"
             fullWidth
             label="Förnamn"
             variant="standard"
@@ -51,6 +95,7 @@ const SignUpForm = () => {
           />
           <TextField
             label="Efternamn"
+            name="lastName"
             variant="standard"
             fullWidth
             placeholder="Ange ditt efternamn"
@@ -58,6 +103,7 @@ const SignUpForm = () => {
           />
           <TextField
             id="standard-basic"
+            name="email"
             label="Email"
             variant="standard"
             fullWidth
@@ -66,6 +112,7 @@ const SignUpForm = () => {
           />
           <TextField
             id="standard-basic"
+            name="password"
             type="password"
             label="Lösenord"
             variant="standard"
