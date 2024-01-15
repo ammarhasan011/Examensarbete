6549433dc3077e0b951e1e6669cebeb545d058c4
Ammar Hasan <ammarhasan011@gmail.com>
2024-01-16 00:55:56 +0100

    felsöker

--- client/src/components/ProfilePage/ProfilePage.tsx
+++ client/src/components/ProfilePage/ProfilePage.tsx
@@ -7,6 +7,7 @@ import PersonIcon from "@mui/icons-material/Person";
 import Button from "@mui/material/Button";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { handleLogoutAndRedirect } from "../Utils/AuthUtils";
 
 // ProfilePage Component
 const ProfilePage = () => {
@@ -22,43 +23,43 @@ const ProfilePage = () => {
   const buttonStyle = { marginTop: 40, margin: "8px 0" };
   const avatarStyle = { backgroundColor: "#1976d2" };
 
-  const handleLogout = async () => {
-    try {
-      // Skicka en DELETE-förfrågan till
-      await axios.delete("/api/users/logout");
-      console.log("User logged out successfully");
-    } catch (error) {
-      console.error(
-        "Logout error:",
-        (error as any).response?.data || (error as any).message
-      );
-    }
-  };
+  // const handleLogout = async () => {
+  //   try {
+  //     // Skicka en DELETE-förfrågan till
+  //     await axios.delete("/api/users/logout");
+  //     console.log("User logged out successfully");
+  //   } catch (error) {
+  //     console.error(
+  //       "Logout error:",
+  //       (error as any).response?.data || (error as any).message
+  //     );
+  //   }
+  // };
 
-  const handleLogoutAndRedirect = async (
-    e: React.FormEvent<HTMLFormElement>
-  ) => {
-    // await handleLogout(e); // Call the handleLogout function
-    e.preventDefault();
+  // const handleLogoutAndRedirect = async (
+  //   e: React.FormEvent<HTMLFormElement>
+  // ) => {
+  //   // await handleLogout(e); // Call the handleLogout function
+  //   e.preventDefault();
 
-    try {
-      await axios.get("/api/users/logout");
-      console.log("User logged out successfully");
+  //   try {
+  //     await axios.get("/api/users/logout");
+  //     console.log("User logged out successfully");
 
-      const response = await axios.get("/api/users/authorize");
-      const userIsLoggedOut = !response.data._id;
-      if (userIsLoggedOut) {
-        navigate("/");
-      } else {
-        console.log("Du är inte inloggad.");
-      }
-    } catch (error) {
-      console.error(
-        "Logout error:",
-        (error as any).response?.data || (error as any).message
-      );
-    }
-  };
+  //     const response = await axios.get("/api/users/authorize");
+  //     const userIsLoggedOut = !response.data._id;
+  //     if (userIsLoggedOut) {
+  //       navigate("/");
+  //     } else {
+  //       console.log("Du är inte inloggad.");
+  //     }
+  //   } catch (error) {
+  //     console.error(
+  //       "Logout error:",
+  //       (error as any).response?.data || (error as any).message
+  //     );
+  //   }
+  // };
 
   return (
     // Render the sign-in form
@@ -76,7 +77,7 @@ const ProfilePage = () => {
               color="primary"
               variant="contained"
               style={buttonStyle}
-              onClick={handleLogout}
+              onClick={() => handleLogoutAndRedirect(navigate)}
             >
               Logga ut
             </Button>
