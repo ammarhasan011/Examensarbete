bd77d68ac7128e1f3da4f4bd2b4242d115f0495d
Ammar Hasan <ammarhasan011@gmail.com>
2024-01-16 20:12:22 +0100

    kommentarer

--- client/src/components/utils/AuthUtils.tsx
+++ client/src/components/utils/AuthUtils.tsx
@@ -1,61 +1,18 @@
-// import axios from "axios";
-// import { handleLogin } from "./signInutils";
-
-// export const handleLogoutAndRedirect = async (navigate: any) => {
-//   try {
-//     // Skicka en DELETE-förfrågan till
-//     await axios.delete("/api/users/logout");
-//     console.log("User logged out successfully");
-
-//     const response = await axios.get("/api/users/authorize");
-//     const userIsLoggedOut = !response.data._id;
-//     if (userIsLoggedOut) {
-//       navigate("/");
-//     } else {
-//       console.log("Du är inte inloggad.");
-//     }
-//   } catch (error) {
-//     console.error(
-//       "Logout error:",
-//       (error as any).response?.data || (error as any).message
-//     );
-//   }
-// };
-
-// // Function for login and redirection
-// export const handleLoginAndRedirect = async (
-//   e: React.FormEvent<HTMLFormElement>
-// ) => {
-//   try {
-//     await handleLogin(e); // Call the handleLogin function
-//     e.preventDefault();
-
-//     // Check if the user is logged in session._id
-//     const response = await axios.get("/api/users/authorize");
-//     const userIsLoggedIn = response.data._id;
-
-//     // If there is a session._id redirect
-//     if (userIsLoggedIn) {
-//       navigate("/profile-page");
-//     } else {
-//       console.log("Du är inte inloggad.");
-//     }
-//   } catch (error) {
-//     console.error(
-//       "Authorize error:",
-//       (error as any).response?.data || (error as any).message
-//     );
-//   }
-// };
+// Import
 import axios from "axios";
 
+// Function to log out and redirect
 export const handleLogoutAndRedirect = async (navigate: any) => {
   try {
+    // Send a DELETE request to log out the user
     await axios.delete("/api/users/logout");
     console.log("User logged out successfully");
 
+    // Fetch user information to check if the user is logged out
     const response = await axios.get("/api/users/authorize");
     const userIsLoggedOut = !response.data._id;
+
+    // If the user is logged out, redirect to the home page
     if (userIsLoggedOut) {
       navigate("/");
     } else {
@@ -69,11 +26,15 @@ export const handleLogoutAndRedirect = async (navigate: any) => {
   }
 };
 
+// Function to log in and redirect
 export const handleLoginAndRedirect = async (navigate: any) => {
   try {
+    // Fetch user information to check if the user is logged in
     const response = await axios.get("/api/users/authorize");
     const userIsLoggedIn = response.data._id;
+    console.log("User logged in successfully");
 
+    // If the user is logged in, redirect to the profile page
     if (userIsLoggedIn) {
       navigate("/profile-page");
     } else {
