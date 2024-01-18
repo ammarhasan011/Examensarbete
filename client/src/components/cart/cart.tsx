7bd0725a714c117b13cdb208e8a3805f7e915f4f
Ammar Hasan <ammarhasan011@gmail.com>
2024-01-18 20:11:23 +0100

    fixar Cart

--- client/src/components/cart/cart.tsx
+++ client/src/components/cart/cart.tsx
@@ -1,9 +1,28 @@
-// import ProfilePage from "../ProfilePage/ProfilePage";
+import { useState } from "react";
+import Product from "../Interfaces/Product";
+
 const Cart = () => {
+  // State för varukorgen
+  const [cart, setCart] = useState([Product]);
+
+  // Funktion för att lägga till en produkt i varukorgen
+  const addToCart = (product: Product) => {
+    // Skapa en kopia av den befintliga varukorgen och lägg till den nya produkten
+    const newCart = [...cart, product];
+    setCart(newCart);
+  };
+
   return (
     <div>
-      <h1>Cart</h1>
-      {/* <ProfilePage /> */}
+      <h1>Varukorg</h1>
+      {/* Loopa igenom varukorgen och visa produkter */}
+      {cart.map((product, index) => (
+        <div key={index}>
+          <p>{product.title}</p>
+          <p>Pris: {product.price} kr</p>
+          {/* Eventuellt annan information om produkten */}
+        </div>
+      ))}
     </div>
   );
 };
