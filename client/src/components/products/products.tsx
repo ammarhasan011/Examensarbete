74fd0e173e02c9dc5e931b9b752198c581f8b849
Ammar Hasan <ammarhasan011@gmail.com>
2024-01-19 11:10:59 +0100

    tog bort tillfälliga varukorgen

--- client/src/components/products/products.tsx
+++ client/src/components/products/products.tsx
@@ -1,4 +1,4 @@
-//Imports
+// Imports
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Product from "../Interfaces/Product";
@@ -7,15 +7,12 @@ import "./products.css";
 
 // Products Component Definition
 const Products = () => {
-  // State to store the list of products
   const [products, setProducts] = useState<Product[]>([]);
-  // Use storedCart within useState to initialize cart
   const [cart, setCart] = useState<Product[]>(() => {
     const storedCart = localStorage.getItem("cart");
     return storedCart ? JSON.parse(storedCart) : [];
   });
 
-  //Products from database
   useEffect(() => {
     axios
       .get("http://localhost:3000/api/products")
@@ -30,16 +27,14 @@ const Products = () => {
   const addToCart = (product: Product) => {
     setCart((prevCart) => {
       const newCart = [...prevCart, product];
-      // Spara varukorgen i localStorage
       localStorage.setItem("cart", JSON.stringify(newCart));
       return newCart;
     });
     console.log(`Lägg till ${product.title} i varukorgen`);
   };
+
   return (
-    // Render component
     <div>
-      {/* Renders a list of products using MultiActionAreaCard */}
       <h1 className="Title">Produkter</h1>
       <div className="CardDiv">
         {products.map((product) => (
@@ -57,12 +52,6 @@ const Products = () => {
           />
         ))}
       </div>
-      {/* <h2>Varukorg</h2>
-      <ul>
-        {cart.map((item) => (
-          <li key={item._id}>{item.title}</li>
-        ))}
-      </ul> */}
     </div>
   );
 };
