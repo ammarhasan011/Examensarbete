0623c943ca568d6c01d4a7ce63b42c74c5131376
Ammar Hasan <ammarhasan011@gmail.com>
2024-01-20 21:19:18 +0100

    line_items

--- server/resources/checkout/checkoutController.js
+++ server/resources/checkout/checkoutController.js
@@ -6,7 +6,15 @@ const createCheckoutSession = async (req, res) => {
 
     const session = await stripe.checkout.sessions.create({
       line_items: cartItems.map((item) => ({
-        price: item.product,
+        price_data: {
+          currency: "sek",
+          product_data: {
+            name: item.name,
+            images: [item.image],
+          },
+          unit_amount: item.price * 100,
+        },
+
         quantity: item.quantity,
       })),
 
@@ -14,6 +22,7 @@ const createCheckoutSession = async (req, res) => {
       success_url: "http://localhost:5173/CONFIRMATION",
       cancel_url: "http://localhost:5173",
     });
+
     res.status(200).json({ url: session.url, sessionId: session.id });
   } catch (error) {
     console.error("Error creating checkout session:", error);
