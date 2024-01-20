f4797696cc9b4e6e7534b501e2df067d8ccc7e6b
Ammar Hasan <ammarhasan011@gmail.com>
2024-01-20 21:48:09 +0100

    tog bort checkout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,7 +9,6 @@ import AboutUs from "./components/AboutUs/AboutUs";
 import Footer from "./components/Footer/Footer";
 import SignUpForm from "./components/UserForms/SignUpForm";
 import ProfilePage from "./components/ProfilePage/ProfilePage";
-import Checkout from "./components/Checkout/Checkout";
 import Confirmation from "./components/Checkout/Confirmation";
 
 // Main component representing the entire application
@@ -26,7 +25,6 @@ const App = () => {
         <Route path="/sign-in" element={<SignInForm />} />
         <Route path="/sign-up" element={<SignUpForm />} />
         <Route path="/profile-page" element={<ProfilePage />} />
-        <Route path="/checkout" element={<Checkout />} />
         <Route path="/confirmation" element={<Confirmation />} />
       </Routes>
       <Footer />
