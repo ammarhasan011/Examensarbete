a8fcc9feb1eeeaec3844bb71a539237582a9e992
Ammar Hasan <ammarhasan011@gmail.com>
2024-01-20 17:15:10 +0100

    skickar cartitems med anrop

--- client/src/components/Checkout/Checkout.tsx
+++ client/src/components/Checkout/Checkout.tsx
@@ -1,23 +1,34 @@
-const Checkout = () => {
+import CartItem from "../Interfaces/CartItem";
+interface CheckoutProps {
+  cartItems: CartItem[];
+}
+// Checkout Component Definition
+const Checkout: React.FC<CheckoutProps> = ({ cartItems }) => {
   async function handlePayment() {
     try {
-      const respons = await fetch("/api/create-checkout-session", {
+      const response = await fetch("/api/create-checkout-session", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(requestBody),
+        body: JSON.stringify({ cartItems }),
       });
-      //  svar
+
+      if (!response.ok) {
+        console.error("The request failed");
+        return;
+      }
+      console.log("The request is good");
     } catch (error) {
       console.error("Ett fel inträffade:", error);
     }
-
-    return (
-      <div>
-        <button onClick={handlePayment}>Betala</button>
-      </div>
-    );
   }
+
+  return (
+    <div>
+      <button onClick={handlePayment}>Betala</button>
+    </div>
+  );
 };
+
 export default Checkout;
