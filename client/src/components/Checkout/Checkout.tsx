ebb2cc1a88956ba2dcdfc86fd4186c193590840b
Ammar Hasan <ammarhasan011@gmail.com>
2024-01-21 22:53:49 +0100

    kommentarer

--- client/src/components/Checkout/Checkout.tsx
+++ client/src/components/Checkout/Checkout.tsx
@@ -5,8 +5,10 @@ interface CheckoutProps {
 }
 // Checkout Component Definition
 const Checkout: React.FC<CheckoutProps> = ({ cartItems }) => {
+  // Function to handle the payment process
   async function handlePayment() {
     try {
+      // Send a POST request to create a checkout session on the server
       const response = await fetch("/api/create-checkout-session", {
         method: "POST",
         headers: {
@@ -17,16 +19,20 @@ const Checkout: React.FC<CheckoutProps> = ({ cartItems }) => {
         }),
       });
 
+      // Check if the server request was successful
       if (!response.ok) {
         console.error("The request failed");
         console.log(cartItems);
         return;
       }
       console.log("The request is good");
+      // Extract session information from the server response
       const { url, sessionId } = await response.json();
+      // Store the session ID in local storage for reference
       localStorage.setItem("session-id", sessionId);
       console.log("sessionId", sessionId);
 
+      // Redirect the user to the Stripe Checkout page
       window.location.href = url;
     } catch (error) {
       console.error("Ett fel inträffade:", error);
