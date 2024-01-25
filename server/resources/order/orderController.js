d84c3dedd16d6a1248c0106726cf966f69b6bebc
Ammar Hasan <ammarhasan011@gmail.com>
2024-01-26 00:12:39 +0100

    respons

--- server/resources/order/orderController.js
+++ server/resources/order/orderController.js
@@ -44,10 +44,12 @@ const addOrder = async (req, res) => {
     });
 
     await order.save();
-    res.status(201).json(order);
+    // res.status(201).json(order);
+    console.log("Order created successfully");
+    return { status: 201, order };
   } catch (err) {
     console.error("Error adding order:", err);
-    res.status(500).json({ error: "Failed to add order." });
+    // res.status(500).json({ error: "Failed to add order." });
   }
 };
 
