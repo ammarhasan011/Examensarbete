85d48cd0f508c6cf55a27e1559330616626da5d4
Ammar Hasan <ammarhasan011@gmail.com>
2024-01-26 14:56:59 +0100

    fetching products

--- client/src/components/Checkout/Confirmation.tsx
+++ client/src/components/Checkout/Confirmation.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 
 interface Product {
@@ -18,46 +18,63 @@ interface OrderData {
 const Confirmation = () => {
   const location = useLocation();
   const [orderData, setOrderData] = useState<OrderData | null>(null);
-  const shouldNotRender = useRef(false);
-
+  const [dataLoaded, setDataLoaded] = useState(false);
+  //   const shouldNotRender = useRef(false);
+  //     if (!shouldNotRender.current) {
+  //     }
   useEffect(() => {
-    const searchParams = new URLSearchParams(location.search);
-    const sessionId = searchParams.get("session_id");
+    const fetchData = async () => {
+      try {
+        const searchParams = new URLSearchParams(location.search);
+        const sessionId = searchParams.get("session_id");
 
-    console.log("sessionId", sessionId);
+        console.log("sessionId", sessionId);
 
-    if (!shouldNotRender.current) {
-      fetch(`/api/confirm-payment?session_id=${sessionId}`)
-        .then((response) => response.json())
-        .then((data: OrderData) => {
-          setOrderData(data);
-          console.log("Order Data:", data);
-          shouldNotRender.current = true;
-        })
-        .catch((error) => {
-          console.error("Error fetching order information:", error);
-        });
-    }
+        const response = await fetch(
+          `/api/confirm-payment?session_id=${sessionId}`
+        );
+        const data: { orderData: OrderData } = await response.json();
+
+        setOrderData(data.orderData);
+        console.log("Order Data:", data.orderData);
+        setDataLoaded(true);
+      } catch (error) {
+        console.error("Error fetching order information:", error);
+      }
+    };
+
+    fetchData();
   }, [location.search]);
+
   console.log("Rendering Order Data:", orderData);
-  if (!orderData) {
+
+  if (!dataLoaded) {
     return <div>Loading...</div>;
   }
 
-  const orderItems = orderData?.products || [];
+  if (!orderData) {
+    return <div>Error loading order data.</div>;
+  }
+
+  const orderItems = orderData.products || [];
 
   return (
     <div>
       <h2>Order Confirmation</h2>
       <p>Order Number: {orderData.orderNumber}</p>
-      <p>Customer ID: {orderData.customerId}</p>
+      <p>Customer Email: {orderData.customerId}</p>
 
       <p>Products:</p>
       <ul>
         {orderItems.map((product) => (
-          <li key={product._id}>
+          <li key={product._id} style={{ marginBottom: "20px" }}>
             Name: {product.name} <br />
-            Image: {product.image} <br />
+            <img
+              style={{ maxWidth: "100px", height: "auto" }}
+              src={product.image}
+              alt={product.name}
+            />{" "}
+            <br />
             Quantity: {product.quantity} <br />
             Price: {product.price} kr <br />
           </li>
