bd7e00e82a90a71e3432262c713ca5c5fc235f0c
Ammar Hasan <ammarhasan011@gmail.com>
2024-01-26 12:09:05 +0100

    remove StrictMode

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,11 @@
 //Import required modules
-import React from "react";
+// import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
 // Use ReactDOM to render the root component of the application
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  <App />
+  // </React.StrictMode>
 );
