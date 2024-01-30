0c665bc55c694e915dafc80fe618a7c5a8ac9bb5
Ammar Hasan <ammarhasan011@gmail.com>
2024-01-30 13:11:38 +0100

    absout us component

--- client/src/components/AboutUs/AboutUs.tsx
+++ client/src/components/AboutUs/AboutUs.tsx
@@ -1,7 +1,47 @@
 const AboutUs = () => {
   return (
     <div>
-      <h1>AboutUs</h1>
+      <h1>Om Oss</h1>
+      <p>
+        MKicks är en unik onlinebutik specialiserad på svårfunna, begränsade och
+        exklusiva sneakers. Vår plattform är dedikerad till att erbjuda
+        sneakerentusiaster ett kuraterat urval av sällsynta skor som ofta är
+        svåra att få tag på genom konventionella medel.
+      </p>
+      <p>
+        Traditionella butiker kämpar ofta med utmaningen att hitta och erbjuda
+        tillgång till sällsynta, begränsade och högkvalitativa sneakers. MKicks
+        föddes ur önskan att överbrygga denna klyfta och föra dessa svårfångade
+        skatter direkt till dina fingertoppar, oavsett deras sällsynthet eller
+        exklusivitet.
+      </p>
+      <p>
+        Med MKicks Online Store strävar vi efter att erbjuda ett bekvämt och
+        tillgängligt sätt för användare att köpa varor från hemmets trygghet.
+      </p>
+      <h2>Sneaker Varumärken</h2>
+      <ul>
+        <li>Nike</li>
+        <li>Adidas</li>
+        <li>Jordan Brand</li>
+        <li>Yeezy</li>
+        <li>Off-White</li>
+        <li>och många fler.</li>
+      </ul>
+      <h2>Funktioner</h2>
+      <ul>
+        <li>
+          Utforska en handplockad samling av sällsynta och begränsade sneakers.
+        </li>
+        <li>
+          Övervinn utmaningarna med att hitta svårfångade skor i traditionella
+          butiker.
+        </li>
+        <li>
+          Säkra ikoniska sneakers som normalt är svåra att hitta och skaffa.
+        </li>
+        <li>Njut av en sömlös och säker utcheckningsprocess.</li>
+      </ul>
     </div>
   );
 };
