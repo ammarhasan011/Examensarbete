65b3d6a3c5a6582961445afdd3344d8c6441124a
Ammar Hasan <ammarhasan011@gmail.com>
2024-01-30 13:56:24 +0100

    product mangment

---
+++ client/src/components/ProductManagement/ProductManagement.tsx
@@ -0,0 +1,12 @@
+const ProductManagement = () => {
+  // Implement your product management logic here
+
+  return (
+    <div>
+      <h1>Product Management</h1>
+      {/* Add, update, and delete product forms/components */}
+    </div>
+  );
+};
+
+export default ProductManagement;
